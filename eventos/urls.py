7d2a1d8ebb202913206d84e78d161bdad2613ae0
Thaís Karolayne <thaiskarolayne14@gmail.com>
2023-07-09 17:35:11 -0300

    atualizacoes 2.0

--- eventos/urls.py
+++ eventos/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('evento.urls')),
-    path('/', include('usuario.urls')),
+    path('', include('evento.urls')),
+    path('', include('usuario.urls')),
 
 ]
